aa97659e9b7a44215371323078149acea95eb571
Myoesis <thomas.barreau1@gmail.com>
2023-01-02 21:43:23 +0100

    eau03 à peaufiner

---
+++ eau03.js
@@ -0,0 +1,37 @@
+// Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
+//Afficher -1 si le paramètre est négatif ou mauvais.
+// Une fonction pour créer la suite de fibonnaci dans un tableau (n=n-1 + n-2) (arreter la fonction quand on arrive à l'index demandé, et afficher le dernier element du tableau)
+
+// mes variables 
+let arg = process.argv[2]
+let fibo = [0,1,1,2]
+
+//Mes fonctions
+let creatfibo = (arg) => {
+    let n
+    if(arg<=fibo.length) {
+        console.log(fibo[arg])
+    } else {
+            for (let i = 4 ; i<=arg ; i++) {
+            let n=fibo[i-1]+fibo[i-2]
+            fibo.push(n)
+
+        }
+        console.log(fibo[arg])
+    }
+}
+
+// Vérifier les erreurs
+let error=(arg) => {
+    if (!arg) {
+        console.log("Il faut entrer un nombre")
+        process.exit()
+    } else if (isNaN(arg)) {
+        console.log("Il faut entrer UN NOMBRE !")
+        process.exit()
+    }
+}
+
+// appeler mon résultat
+error(arg)
+creatfibo(arg)
