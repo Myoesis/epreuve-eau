e57ca0bddeb2505b2394f3392a937b447199d8b6
Myoesis <thomas.barreau1@gmail.com>
2023-01-30 13:05:57 +0100

    eau15 ok

---
+++ eau15.js
@@ -0,0 +1,13 @@
+// consigne : 
+console.log ("j'ai temriné l'épreuve de l'eau et c'était challengant et  j'ai eu qq noeuds au cerveau")
+// mes variables 
+
+
+
+//Mes fonctions
+
+// Vérifier les erreurs
+
+
+// appeler mon résultat
+
