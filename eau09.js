9ea84ae1a68922d8914b6dae5b8838839f45fc18
Myoesis <thomas.barreau1@gmail.com>
2023-01-31 15:46:10 +0100

    correction de synthaxe

--- eau09.js
+++ eau09.js
@@ -15,7 +15,8 @@ betweenMinMax=(num1,num2)=> {
     for (let i = num2 ; i < num1 ; i++) {
         answer += `${i} `
     } 
-} else if (num1<num2) { for (let i = num1 ; i < num2 ; i++) {
+} else if (num1<num2) { 
+    for (let i = num1 ; i < num2 ; i++) {
         answer += `${i} `
     }
 }
