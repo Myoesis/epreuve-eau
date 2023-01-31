82683fc2fa0f86cd589837b361c6532838aaf11e
Myoesis <thomas.barreau1@gmail.com>
2023-01-31 16:19:33 +0100

    création de la fonction error

--- eau11.js
+++ eau11.js
@@ -1,14 +1,5 @@
 // consigne : Créez un programme qui affiche la différence minimum absolue entre deux éléments d’un tableau constitué uniquement de nombres.let error = () => {
-    for (let i=0 ; i<argArray.length ; i++) {
-        if (isNaN(parseInt(argArray[i]))) {
-            console.log("error")
-            process.exit()
-        }
-    } 
-    if (process.argv[2]== undefined) {
-        console.log("error")                           // j'avais essayé de rentrer cette condition dans la boucle, mais ca ne marchait pas. Pourquoi?
-        process.exit()
-    }
+   
     
 // Nombres négatifs acceptés.
 
@@ -37,9 +28,18 @@ let compare = () => {
 } console.log(diff)
 }
 // Vérifier les erreurs
-
-
-
+let error = () => {
+for (let i=0 ; i<argArray.length ; i++) {
+    if (isNaN(parseInt(argArray[i]))) {
+        console.log("error")
+        process.exit()
+    }
+} 
+if (process.argv[2]== undefined) {
+    console.log("error")                           // j'avais essayé de rentrer cette condition dans la boucle, mais ca ne marchait pas. Pourquoi?
+    process.exit()
+}
+}
 // appeler mon résultat
 error()
 compare()
